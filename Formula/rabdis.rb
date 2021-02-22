a9fe9fbf96835db4250b32583fc54a6ae57433fe
Julien Breux <julien@golang.fr>
2021-02-22 10:35:21 +0100

    Brew formula update for rabdis version v0.10.0

--- Formula/rabdis.rb
+++ Formula/rabdis.rb
@@ -5,24 +5,24 @@
 class Rabdis < Formula
   desc "🐰 Rabdis – Play with redis keys from RabbitMQ messages"
   homepage "https://github.com/julienbreux/rabdis/"
-  version "0.4.0"
+  version "0.10.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/JulienBreux/rabdis/releases/download/v0.4.0/rabdis_0.4.0_Darwin_x86_64.tar.gz"
-    sha256 "6529be8b004b7a2462258029dd6c15e5b415525a4f4e738d4f5c5039f6f119af"
+    url "https://github.com/JulienBreux/rabdis/releases/download/v0.10.0/rabdis_0.10.0_Darwin_x86_64.tar.gz"
+    sha256 "1f1d6f9a586607fed17843825ccb220dc3187b991030dee56bb92c8cf4dafcbc"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/JulienBreux/rabdis/releases/download/v0.4.0/rabdis_0.4.0_Darwin_arm64.tar.gz"
-    sha256 "4ed90da365621a65714b8f0c7de54db1660b4085e728f101e1ee862824c64bda"
+    url "https://github.com/JulienBreux/rabdis/releases/download/v0.10.0/rabdis_0.10.0_Darwin_arm64.tar.gz"
+    sha256 "cda36939e222946a11433a21fe57dd3febf13e044ff8eafcad549bdbd5c05966"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/JulienBreux/rabdis/releases/download/v0.4.0/rabdis_0.4.0_Linux_x86_64.tar.gz"
-    sha256 "24a22668dd4833b7a5c6429e08ca0d89c7ae1e6b16189b3365e1f8229a0bd536"
+    url "https://github.com/JulienBreux/rabdis/releases/download/v0.10.0/rabdis_0.10.0_Linux_x86_64.tar.gz"
+    sha256 "617a31697a4a62905a29394748b18543abb0759f5479003687b363f1dcf2e3db"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/JulienBreux/rabdis/releases/download/v0.4.0/rabdis_0.4.0_Linux_arm64.tar.gz"
-    sha256 "d1aec10fe300151016647cd97cde943c9b4b8a25a70376fed9693c87d24d255e"
+    url "https://github.com/JulienBreux/rabdis/releases/download/v0.10.0/rabdis_0.10.0_Linux_arm64.tar.gz"
+    sha256 "5662dc74c5e80ac7efd0762c1c4d8198fbb9a58c53c76c4bec4be9fce590ff20"
   end
 
   def install
